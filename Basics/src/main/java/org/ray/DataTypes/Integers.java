fd6e20b1dec3da508af05f4cb5f8a8784d517ace
Ankur Ray <143903292+iam-ankur-ray@users.noreply.github.com>
2026-02-28 11:13:42 +0530

    Integer data type and how to run it on main to show the value

---
+++ Basics/src/main/java/org/ray/DataTypes/Integers.java
@@ -0,0 +1,18 @@
+package org.ray.DataTypes;
+
+public class Integers {
+    //We can define integers and related data type by this line of
+    // DataType ReferenceName = Value
+
+    //For example
+
+    int salary = 85000;
+
+
+    // Now to get this value in our main app we can create the object of this class by get method we can
+    // call this reference to print the value of salary
+
+    public int getSalary() {
+        return this.salary; //Here the keyword this refer to the line 9 where we first defined the salary
+    }
+}
--- Basics/src/main/java/org/ray/Main.java
+++ Basics/src/main/java/org/ray/Main.java
@@ -1,17 +1,16 @@
 package org.ray;
 
+import org.ray.DataTypes.Integers;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+       Integers name = new Integers();  // Here we created the object of Class Integers which is present in DataTypes
+        int salary = name.getSalary(); // Referred the class object in salary
+        System.out.println("Salary is :" + salary); // Finally printed the salary by concatenating the string and value
     }
+
 }
