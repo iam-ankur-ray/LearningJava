c551b8ddfbd8e282ad221a52129282228d4606f7
Ankur Ray <143903292+iam-ankur-ray@users.noreply.github.com>
2026-03-16 21:40:44 +0530

    switch case

--- Basics/src/main/java/org/ray/Main.java
+++ Basics/src/main/java/org/ray/Main.java
@@ -1,18 +1,42 @@
 package org.ray;
 
-import org.ray.DataTypes.CharacterType;
-import org.ray.DataTypes.IntegersType;
-import org.ray.DataTypes.StringType;
+import java.util.List;
 
 public class Main {
+
+
     public static void main(String[] args) {
-       IntegersType name = new IntegersType();  // Here we created the object of Class Integers which is present in DataTypes
-        int salary = name.getSalary(); // Referred the class object in salary
-        System.out.println("Salary is :" + salary); // Finally printed the salary by concatenating the string and value
-        StringType person = new StringType("Ankur","ankur@ray.com","Delhi");
-        System.out.println(person);
-        CharacterType characterType = new CharacterType();
-        characterType.printAlphabets();
-  }
+
+        List<String> students = List.of("Name1", "Name2", "Name3", "Name4", "Name5", "Name6");
+        getListStudents(students);
+        System.out.println("-".repeat(75));
+        getStudents();
+
+    }
+
+
+    private static void getStudents() {
+        for (int i = 1; i <= 5; i++) {
+            Student students = new Student(
+                    switch (i) {
+                        case 1 -> "Name1";
+                        case 2 -> "Name2";
+                        case 3 -> "Name3";
+                        case 4 -> "Name4";
+                        case 5 -> "Name5";
+                        default -> "Anonymous";
+                    }, "00" + i, "Java Master Class", "2" + i
+
+            );
+            System.out.println(students);
+        }
+    }
+
+    private static void getListStudents(List<String> names) {
+        for (int i = 0; i < names.toArray().length; i++) {
+            Student students = new Student(names.get(i), "SBN00" + (i + 1), "Java Master Class", "2" + i);
+            System.out.println(students);
+        }
+    }
 
 }
---
+++ Basics/src/main/java/org/ray/Student.java
@@ -0,0 +1,5 @@
+package org.ray;
+
+public record Student(String name, String rollNumber, String course, String age ) {
+}
+
